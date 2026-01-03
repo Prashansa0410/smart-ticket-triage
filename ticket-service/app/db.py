4a4633f672058cfcbc1900c9f838713abffa2f51
Prashansa0410 <prashgupta0410@gmail.com>
2026-01-03 16:42:59 +0530

    Add DATABASE_URL fallback and fix Postgres config

--- ticket-service/app/db.py
+++ ticket-service/app/db.py
@@ -2,7 +2,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./tickets.db"  # local fallback
+)
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
